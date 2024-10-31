deb39c8943da4b0368040285096b2dca88dba1cc
rani-neo <ishika.rashmi@gmail.com>
2024-10-31 15:36:05 +1100

    chalange1#

--- app.js
+++ app.js
@@ -64,14 +64,183 @@
 // 
 // // If year is undefined (was not passed), it should be assigned with a default value of 'year unknown'.
 // Correct function definition
-function printBookInfo(title, author, year = 'year unknown') {
-    console.log(`${title} by ${author}, ${year}`);
-}
+// function printBookInfo(title, author, year = 'year unknown') {
+    // console.log(`${title} by ${author}, ${year}`);
+// }
+// 
+//Call the function with different arguments
+// // printBookInfo('Algorithms', 'Robert Sedgewick', 2011); // Output: "Algorithms by Robert Sedgewick, 2011"
+// // printBookInfo('Crafting Interpreters', 'Robert Nystrom'); // Output: "Crafting Interpreters by Robert Nystrom, year unknown"
+// The Spread Operator﻿
+// 3.1﻿
+// // // Each book object has the author property, which stores an array of strings (author names) if there are multiple authors, or a single string (author name) if there is just one author.
+// 
+ // Declare an array called bookAuthors, and fill it with authors of the first two books from the books array. The bookAuthors array should have just one level (no nested arrays).
 
-// Call the function with different arguments
-printBookInfo('Algorithms', 'Robert Sedgewick', 2011); // Output: "Algorithms by Robert Sedgewick, 2011"
-printBookInfo('Crafting Interpreters', 'Robert Nystrom'); // Output: "Crafting Interpreters by Robert Nystrom, year unknown"
+//  const bookAuthors = [...books[0].author, ...books[1].author];
+//  console.log(bookAuthors);
+// 3.2﻿
+// // Write a function called spellWord that accepts a single string as an argument. This function should log to the console each letter of the argument separated by a space.
+// spellWord('JavaScript')
+// function spellWord(word) {
+  // console.log(word);
+// }
+// Rest Pattern and Parameters﻿
+// 4.1﻿
+// // // // Destructure the keywords property (array) of the first book from the books array into variables called mainKeyword and rest. The first keyword should be assigned to mainKeyword, and the rest of the keywords should be assigned to the rest variable (it should be an array).
+// const [mainKeyword, ...rest] = books[0].keywords;
+// console.log(mainKeyword, ...rest);
+// 4.2 Destructure the second book from the books array into a variable called bookPublisher. The bookPublisher variable should be assigned with the value of the publisher property of the book object. Assign the rest of the properties to the restOfTheBook variable.
+// const { publisher: bookPublisher, ...restOfTheBook } = books[1];
+// console.log(bookPublisher);         // Output: 'The MIT Press'
+// console.log(restOfTheBook);
+// 4.3﻿
+// // // // Write a function called printBookAuthorsCount that has two parameters called title and authors. The authors parameter should accept any number of arguments. This function should log to the console a string formatted like that: "The book "${title}" has ${authors.length} authors".
+// function printBookAuthorsCount(title, ...authors) {
+  // console.log(`The book "${title}" has ${authors.length} authors"`)
+// }
+// printBookAuthorsCount('Algorithms', 'Robert Sedgewick', 'Kevin Wayne'); 
+// Short Circuiting (&& and ||)﻿
+// 5.1﻿
+// // Some of the book objects have the programmingLanguage property, which specifies what programming language is used in the book, for example
+// 
+// {
+  // title: 'Algorithms',
+  // author: ['Robert Sedgewick', 'Kevin Wayne'],
+  // ...
+// programmingLanguage: 'Java',     // <-- HERE
+// }
+// Write a function called hasExamplesInJava that takes a book object from the books array as an argument. This function should return true if the book uses Java, or a string 'no data available' if it uses other language or no programming language at all.
+// function hasExamplesInJava(book) {
+   //Use short-circuiting to check the programmingLanguage property
+    // return book.programmingLanguage === 'Java' || 'no data available';
+// }
+// console.log(hasExamplesInJava(books[0])); // Output: true
+// console.log(hasExamplesInJava(books[1])); // Output: 'no data available'
+// console.log(hasExamplesInJava(books[2])); // Output: 'no data available'
+// console.log(hasExamplesInJava(books[3])); // Output: 'no data available'
+// 5.2﻿
+// // // // Some of the book objects have the onlineContent property, which is either true or false. Loop over the books array, and for the books that provide online content, log to the console a string in this format: "${title}" provides online content. Use short-circuiting.
+// 
+// {
+  // title: 'Operating System Concepts',
+ // ... removed for clarity
+  // onlineContent: false,          // <-- HERE
+// },
+// Loop over the books array
+// for (const book of books) {
+    //Use short-circuiting to check for onlineContent
+    // book.onlineContent && console.log(`${book.title} provides online content.`);
+// }
+// 6.1﻿
+// // // There are objects in the books array that don't have the onlineContent property at all. Loop over the books array, and log a string to the console in this format: "${title}" provides no data about its online content.
+// 
+// show example solution
+//  {...}
+// for (let i = 0; i < books.length; i++) {
+  // // books[i].onlineContent ?? console.log(`"${books[i].title} provides no data about its online content"`);
+// }
+// Logical Assignments Operators﻿
+// 7.1﻿
+// // // Some of the book objects from the books array are missing the edition property. Loop over the books array, and assign this property with a number 1 (if it doesn't already exist). Use logical assignment operators.
 
+// Use nullish assignment to set edition to 1 if it's missing
+// books.forEach(book => {
+  // book.edition ??= 1;
+// });
+// console.log(books);
+// 7.2﻿
+// // // Some of the book objects from the books array have the highlighted property, which by default is set to true. Iterate over the books array, and if the thirdParty.goodreads.rating property is less than 4.2, reassign it with false.
+// 
+// Use the &&= operator (tip: you may also need the ! operator)
+// books.forEach(book => {
+  // If the rating is less than 4.2, set highlighted to false
+ // book.highlighted &&= !(book.thirdParty.goodreads.rating < 4.2);
+// });
+// 
+// console.log(books);
 
+///////////////////////////////////////
+// Coding Challenge #1
 
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
 
+Suppose we get data from a web service about a certain game (below). In this challenge 
+we're gonna work with the data. So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field 
+players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's 
+name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new 
+array ('players1Final') containing all the original team1 players plus 'Thiago', 
+'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 
+'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names 
+(NOT an array) and prints each of them to the console, along with the number of goals 
+that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is 
+more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call 
+the function again with players from game.scored
+
+GOOD LUCK 😀*/ 
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnabry',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnabry', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    draw: 3.25,
+    team2: 6.5,
+  },
+};
+//1. Create one player array for each team (variables 'players1' and 'players2')
+const [players1, players2] = game.players;
+console.log(players1, players2);
+//2. The first player in any player array is the goalkeeper and the others are field 
+//players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's 
+//const [gk, ...fieldplayer] = players1;
+//console.log(gk, fieldplayer);
+//3. Create an array 'allPlayers' containing all players of both teams (22 players)
+// const allPlayers = [...players1, ...players2];
+// console.log(allPlayers);
+//4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new
+//array ('players1Final') containing all the original team1 players plus 'Thiago', 
+//'Coutinho' and 'Perisic'
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisis'];
+console.log(players1Final);
--- script.js
+++ script.js
@@ -7,32 +7,138 @@ const restaurant = {
     starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
     mainMenu: ['Pizza', 'Pasta', 'Risotto'],
     openingHours: {
-  thu: {
-    open: 12,
-    close: 22,
-  },
-  fri: {
-    open: 11,
-    close: 23,
-  },
-  sat: {
-    open: 0, // Open 24 hours
-    close: 24,
-  },
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0, // Open 24 hours
+            close: 24,
+        },
     },
     
 
     order: function (starterIndex, mainIndex) {
         return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
     },
-    orderDelivery: function ({starterIndex = 1, mainIndex = 0, time = '20:00', address}) {
+    orderDelivery: function ({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
         console.log(`order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}}`);
     },
     orderPasta: function (ing1, ing2, ing3) {
         console.log(`Here is your declicious pasta with ${ing1}, ${ing2}, ${ing3}`);
+    },
+    orderPizza: function (mainIngredient, ...otherIngredients) {
+        console.log(mainIngredient);
+        console.log(otherIngredients);
     }
-};
 
+};
+/////////////////
+/* Logical Assignment Operators
+const rest1 = {
+    name: 'Capri',    
+    // numGuests: 20,   
+    numGuests: 0,
+};
+const rest2 = {
+    name: 'La Piazza',
+    owner: 'Giovanni Rossi',
+};
+// OR assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+// rest1.numGuests || = 10;
+// rest2.numGuests || = 10;
+// console.log(rest1);
+// console.log(rest2);
+
+// nullish assignment operator (null or undefined)
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+// And assignment operator
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+console.log(rest1);
+console.log(rest2);
+
+/* 
+/////////////////
+ // The Nullish coalescing Operator
+restaurant.numGuests = 0;
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+// Nullish: null and undefined (Not 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
+
+/*
+/////////////////////
+// Short Circuiting (&& and ||)
+console.log('... OR --- ');
+// Use any data type, return any data type, short-circuiting
+console.log(3 || 'Jons');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+restaurant.numGuests = 23;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('--- And ---');
+console.log(0 && 'Jonas');
+console.log(7 && 'jonas');
+console.log('Hello' && null && 'jonas');
+// practicle example 
+if (restaurant.orderPizza) {
+    restaurant.orderPizza('mushrooms', 'spinach');
+}
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+
+
+/*
+/////////////// 
+// Rest patterns and Parameters
+//1) Destructuring
+// Spread, because on Right side of = 
+const arr = [1, 2, ...[3, 4]];
+// Rest, Because on left side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+const [pizza, , risotto, ...otherFood] = [...restaurant.mainMenu, ...restaurant.starterMenu]
+console.log(pizza, risotto, otherFood);
+
+//Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+// 2) Functions
+const add = function (...numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) sum +=
+        numbers[i];
+    console.log(sum);
+};
+add(2, 3)
+add(5, 3, 7, 2)
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms');
+
+/*
+////////////
+ // The Spread Operator(...)
 const arr = [7, 8, 9];
 const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
 console.log(badNewArr);
@@ -61,8 +167,8 @@ console.log(...str);
 // console.log(`${...str} Schmedtmann`);
 // Real- world example
 const ingredients = [prompt("Let's make pasta! Ingredients 1?"),
-// prompt('Ingredient 2?'),
-// prompt('Ingredient 3'),
+//  prompt('Ingredient 2?'),
+//  prompt('Ingredient 3'),
 ];
 console.log(ingredients);
 restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
