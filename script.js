5c3e235db8c2fe2b5ea0415ca52c538876bc38d0
rani-neo <ishika.rashmi@gmail.com>
2024-11-01 16:29:27 +1100

    Looping Objects

--- script.js
+++ script.js
@@ -42,6 +42,29 @@ const restaurant = {
     }
 
 };
+// Property names
+let openStr = ''; // Correctly declare and initialize the variable
+
+const properties = Object.keys(openingHours);
+console.log(properties);
+console.log(`We are open on ${properties.length} days`);
+for (const day of properties) {
+    openStr += `${day}, `;
+}
+console.log(openStr);
+// property values
+const values = Object.values(openingHours);
+console.log(values);
+// Entire object 
+const entries = Object.entries(openingHours);
+//console.log(entries);
+// [key, value]
+for (const [day, {open, close}] of entries) {
+    console.log(`On ${day} we open at ${open} and close at ${close}`);
+}
+/* 
+/////////////////////////
+// Optional Chaining
 if (restaurant.openingHours && restaurant.openingHours.mon)
     console.log(restaurant.openingHours.mon.open);
 // console.log(restaurant.openingHours.mom.open);
