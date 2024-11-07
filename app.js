2cbd3af83622aec71484552c7712c13ed7723556
rani-neo <ishika.rashmi@gmail.com>
2024-11-07 14:01:19 +1100

    Sets coding

--- app.js
+++ app.js
@@ -400,8 +400,8 @@ const game = {
 
 // 1. Loop over the game.scored array and print each player name 
 // to the console, along with the goal number (Example: "Goal 1: Lewandowski")
-for (const [i, player] of game.scored.entries())
-  console.log(`Goal ${i + 1}: ${player}`);
+// for (const [i, player] of game.scored.entries())
+  // console.log(`Goal ${i + 1}: ${player}`);
 //2. Use a loop to calculate the average odd and log it to the console (We already studied 
 //how to calculate averages, you can go check if you don't remember)
 const odds = Object.values(game.odds)
@@ -409,6 +409,17 @@ let average = 0;
 for (const odd of odds) average += odd;
 average /= odds.length;
 console.log(average);
-
-
-
+// 3. Print the 3 odds to the console, but in a nice formatted 
+// way, exaclty like this:
+     // Odd of victory Bayern Munich: 1.33
+      //Odd of draw: 3.25
+      //Odd of victory Borrussia Dortmund: 6.5
+//Get the team names directly from the game object, don't 
+// hardcode them (except for 
+//"draw"). HINT: Note how the odds and the game objects have the 
+// same property names 😉
+// for (const [team, odd] of Object.entries(game.odds)) {
+    // //  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+    // console.log(`Odd of ${teamStr}: ${odd}`);
+// }
+// 
--- script.js
+++ script.js
@@ -42,7 +42,28 @@ const restaurant = {
         console.log(otherIngredients);
     },
 };
+const ordersSet = new Set(['Pasta', 'Pizza', 'Pizza', 'Risotto', 'Pasta', 'Pizza']);
+console.log(ordersSet);
+console.log(new Set('Jonas'));
+console.log(ordersSet.size);
+console.log(ordersSet.has('Pizza'));
+console.log(ordersSet.has('Bread'));
+ordersSet.add('Garlic Bread');
+ordersSet.add('Garlic Bread');
+ordersSet.delete('Risotto');
+//orderSet.clear();
+console.log(ordersSet);
+for (const order of ordersSet) console.log(order);
+//Examples 
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+console.log(new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size);
+console.log(new Set('jonasschmedtmann').size);
 
+
+/*
+/////////////////
 //Looping Objects: Object keys, Values, and Entries
 // Property names
 // let openStr = ''; // Correctly declare and initialize the variable
